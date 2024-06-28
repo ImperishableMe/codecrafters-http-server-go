03576967772639453ba35f54093f0917082e0bb4
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2024-06-28 23:57:29 +0600

    Create http package and make the main slim

--- app/http/server.go
+++ app/http/server.go
@@ -1,87 +1,12 @@
-package main
+package http
 
 import (
 	"fmt"
 	"io"
 	"net"
 	"os"
-	"path/filepath"
-	"strconv"
 	"strings"
 )
-
-func main() {
-	fmt.Println("Logs from your program will appear here!")
-
-	serverOptions := ServerOptions{}
-	if len(os.Args) >= 3 {
-		serverOptions.FileServerRoot = os.Args[2]
-	}
-	s := NewServer(serverOptions)
-
-	// Register the handlers
-	s.Register("GET /", func(r *Request, w ResponseWriter) {
-		w.Write([]byte(""))
-	})
-	s.Register("GET /user-agent", func(r *Request, w ResponseWriter) {
-		userAgent, ok := r.Headers["user-agent"]
-		if !ok {
-			w.WriteHeader(400)
-			w.Write([]byte("User-Agent header is required"))
-			return
-		}
-		w.Write([]byte(userAgent))
-	})
-	s.Register("GET /echo/{str}", func(r *Request, w ResponseWriter) {
-		str, _ := strings.CutPrefix(r.Path, "/echo/")
-		fmt.Println("echoing back: ", str)
-		w.Write([]byte(str))
-	})
-	s.Register("GET /files/{path}", func(r *Request, w ResponseWriter) {
-		path, _ := strings.CutPrefix(r.Path, "/files/")
-		fmt.Println("Asking for file ", path)
-		filePath := filepath.Join(s.fileServerRoot, path)
-		fileBody, err := os.ReadFile(filePath)
-		if err != nil {
-			w.WriteHeader(404)
-			w.Write(nil)
-			return
-		}
-		w.Headers()["Content-Type"] = "application/octet-stream"
-		w.Write(fileBody)
-	})
-	s.Register("POST /files/{path}", func(r *Request, w ResponseWriter) {
-		fmt.Println(r.Headers)
-		path, _ := strings.CutPrefix(r.Path, "/files/")
-		filePath := filepath.Join(s.fileServerRoot, path)
-		file, err := os.Create(filePath)
-		if err != nil {
-			w.WriteHeader(400)
-			return
-		}
-		defer file.Close()
-
-		lenStr, ok := r.GetHeader("Content-Length")
-		if !ok {
-			w.WriteHeader(400)
-			w.Write([]byte("missing Content-Length header."))
-			return
-		}
-		fileSize, _ := strconv.Atoi(lenStr)
-		fmt.Println("Reading total file size of ", fileSize)
-
-		written, err := io.CopyN(file, r.Body, int64(fileSize))
-		fmt.Println("Read ", written, " bytes")
-		if err != nil || written != int64(fileSize) {
-			w.WriteHeader(400)
-			w.Write([]byte(fmt.Sprintf("couldn't write the whole content. Wrote %d, expected %d", written, fileSize)))
-			return
-		}
-		w.WriteHeader(201)
-	})
-
-	s.ListenAndServe()
-}
 
 type Handler struct {
 	pat     pattern
@@ -108,9 +33,9 @@ func (h Handler) matches(pat pattern) bool {
 }
 
 type Server struct {
-	fileServerRoot string
-	handlers       []Handler
-	port           int
+	FileServerRoot string
+	Handlers       []Handler
+	Port           int
 }
 
 func NewServer(options ServerOptions) Server {
@@ -122,9 +47,9 @@ func NewServer(options ServerOptions) Server {
 		options.Port = 4221
 	}
 	return Server{
-		fileServerRoot: options.FileServerRoot,
-		handlers:       make([]Handler, 0),
-		port:           options.Port,
+		FileServerRoot: options.FileServerRoot,
+		Handlers:       make([]Handler, 0),
+		Port:           options.Port,
 	}
 }
 
@@ -134,12 +59,12 @@ func (s *Server) Register(pat string, h HandlerFunc) error {
 	if err != nil {
 		return err
 	}
-	s.handlers = append(s.handlers, Handler{pat: p, handler: h})
+	s.Handlers = append(s.Handlers, Handler{pat: p, handler: h})
 	return nil
 }
 
 func (s *Server) ListenAndServe() {
-	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", s.port))
+	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", s.Port))
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
@@ -183,7 +108,7 @@ func (s *Server) findHandler(path string) HandlerFunc {
 		return NotFoundHandler
 	}
 	fmt.Println("trying to match pattern..", pat)
-	for _, h := range s.handlers {
+	for _, h := range s.Handlers {
 		if h.matches(pat) {
 			return h.handler
 		}
---
+++ app/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/codecrafters-io/http-server-starter-go/app/http"
+)
+
+func main() {
+	fmt.Println("Logs from your program will appear here!")
+
+	serverOptions := http.ServerOptions{}
+	if len(os.Args) >= 3 {
+		serverOptions.FileServerRoot = os.Args[2]
+	}
+	s := http.NewServer(serverOptions)
+
+	registerHandlers(&s)
+
+	// blocks indefinitely
+	s.ListenAndServe()
+}
+
+func registerHandlers(s *http.Server) {
+
+	// Register the handlers
+	s.Register("GET /", func(r *http.Request, w http.ResponseWriter) {
+		w.Write([]byte(""))
+	})
+	s.Register("GET /user-agent", func(r *http.Request, w http.ResponseWriter) {
+		userAgent, ok := r.Headers["user-agent"]
+		if !ok {
+			w.WriteHeader(400)
+			w.Write([]byte("User-Agent header is required"))
+			return
+		}
+		w.Write([]byte(userAgent))
+	})
+	s.Register("GET /echo/{str}", func(r *http.Request, w http.ResponseWriter) {
+		str, _ := strings.CutPrefix(r.Path, "/echo/")
+		fmt.Println("echoing back: ", str)
+		w.Write([]byte(str))
+	})
+	s.Register("GET /files/{path}", func(r *http.Request, w http.ResponseWriter) {
+		path, _ := strings.CutPrefix(r.Path, "/files/")
+		fmt.Println("Asking for file ", path)
+		filePath := filepath.Join(s.FileServerRoot, path)
+		fileBody, err := os.ReadFile(filePath)
+		if err != nil {
+			w.WriteHeader(404)
+			w.Write(nil)
+			return
+		}
+		w.Headers()["Content-Type"] = "application/octet-stream"
+		w.Write(fileBody)
+	})
+	s.Register("POST /files/{path}", func(r *http.Request, w http.ResponseWriter) {
+		fmt.Println(r.Headers)
+		path, _ := strings.CutPrefix(r.Path, "/files/")
+		filePath := filepath.Join(s.FileServerRoot, path)
+		file, err := os.Create(filePath)
+		if err != nil {
+			w.WriteHeader(400)
+			return
+		}
+		defer file.Close()
+
+		lenStr, ok := r.GetHeader("Content-Length")
+		if !ok {
+			w.WriteHeader(400)
+			w.Write([]byte("missing Content-Length header."))
+			return
+		}
+		fileSize, _ := strconv.Atoi(lenStr)
+		fmt.Println("Reading total file size of ", fileSize)
+
+		written, err := io.CopyN(file, r.Body, int64(fileSize))
+		fmt.Println("Read ", written, " bytes")
+		if err != nil || written != int64(fileSize) {
+			w.WriteHeader(400)
+			w.Write([]byte(fmt.Sprintf("couldn't write the whole content. Wrote %d, expected %d", written, fileSize)))
+			return
+		}
+		w.WriteHeader(201)
+	})
+}
